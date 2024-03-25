98d51ddf06486ca1c9019c38545dc97a9a086039
AbduGad <abdo79gad@gmail.com>
2024-03-25 21:16:11 +0200

    test

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,36 +1,37 @@
 #!/usr/bin/python3
-"""script for parsing web data from an api
-"""
+"""API usage"""
+import json
+import requests
+import sys
+
+
+def API():
+    """Get user name and undone tasks list"""
+    # getting user name
+    baseUrl = "https://jsonplaceholder.typicode.com/"
+    usersUrl = baseUrl + "users/" + sys.argv[1]
+    tasksUrl = baseUrl + "todos"
+    with requests.get(usersUrl) as marko:
+        polo = marko.json()
+        name = polo["name"]
+
+    # Getting tasks list
+    with requests.get(tasksUrl) as marko:
+        polo = marko.json()
+        tasks = 0
+        done = 0
+        toDo = []
+        for elem in polo:
+            if elem["userId"] == int(sys.argv[1]):
+                tasks = tasks + 1
+            if (elem["userId"] == int(sys.argv[1]) and
+                    elem["completed"] is True):
+                done = done + 1
+                toDo.append(elem["title"])
+    print("Employee {} is done with tasks({}/{}):".format(name, done, tasks))
+    for task in toDo:
+        print("\t", task)
+
+
 if __name__ == "__main__":
-    import json
-    import requests
-    import sys
-    base_url = 'https://jsonplaceholder.typicode.com/'
-    try:
-        employee_id = sys.argv[1]
-    except:
-        print('Usage: {} employee_id'.format(sys.argv[0]))
-        exit(1)
-
-    # grab the info about the user
-    url = base_url + 'users?id={}'.format(employee_id)
-    response = requests.get(url)
-    user = json.loads(response.text)
-    name = user[0].get('name')
-
-    # grab the info about the user's tasks
-    url = base_url + 'todos?userId={}'.format(employee_id)
-    response = requests.get(url)
-    objs = json.loads(response.text)
-    completed = 0
-    completed_tasks = []
-    for obj in objs:
-        if obj.get('completed'):
-            completed_tasks.append(obj)
-            completed += 1
-
-    # print the output about user's task completion
-    print("{} is done with tasks({}/{}):".format(name, completed, len(objs)))
-    # print the output title of completed tasks
-    for task in completed_tasks:
-        print("\t {}".format(task.get('title')))
+    API()
